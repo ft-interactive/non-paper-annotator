55c7796db9532e49eae74174c7929cf1f8739c15
David Blood <davidbjourno@users.noreply.github.com>
2017-04-20 17:55:08 +0100

    Initial commit

---
+++ config/article.js
@@ -0,0 +1,100 @@
+export default () => ({ // eslint-disable-line
+
+  // link file UUID
+  id: '9b6dbf92-1533-11e7-80f4-13e067d5072c',
+
+  // canonical URL of the published page
+  // https://ig.ft.com/sites/federal-reserve-march-meeting-2017 get filled in by the ./configure script
+  url: 'https://ig.ft.com/eu-brexit-guidelines-annotated',
+
+  // To set an exact publish date do this:
+  //       new Date('2016-05-17T17:11:22Z')
+  publishedDate: new Date(),
+
+  headline: 'The EU’s Brexit negotiation guidance decoded',
+
+  // summary === standfirst (Summary is what the content API calls it)
+  summary: 'The EU’s Brexit negotiation guidelines, with FT correspondents’ notes and comments',
+
+  topic: {
+    name: 'Brexit',
+    url: 'https://www.ft.com/topics/themes/Brexit',
+  },
+
+  relatedArticle: {
+    text: '',
+    url: '',
+  },
+
+  mainImage: {
+    title: '',
+    description: '',
+    url: '',
+    width: 2048, // ensure correct width
+    height: 1152, // ensure correct height
+  },
+
+  // Byline can by a plain string, markdown, or array of authors
+  // if array of authors, url is optional
+  byline: [
+    { name: 'Alex Barker', url: 'https://www.ft.com/stream/authorsId/Q0ItMDAwMDY3Mg==-QXV0aG9ycw==' },
+    { name: 'Martin Stabe', url: 'https://www.ft.com/martin-stabe' },
+  ],
+
+  // Appears in the HTML <title>
+  title: 'The EU Brexit guidelines decoded',
+
+  // meta data
+  description: 'The EU’s Brexit negotiation guidelines with comments from the FT',
+
+  /*
+  TODO: Select Twitter card type -
+        summary or summary_large_image
+
+        Twitter card docs:
+        https://dev.twitter.com/cards/markup
+  */
+  twitterCard: 'summary',
+
+  /*
+  TODO: Do you want to tweak any of the
+        optional social meta data?
+  */
+  // General social
+ socialImage: 'https://www.ft.com/__origami/service/image/v2/images/raw/http%3A%2F%2Fcom.ft.imagepublish.prod.s3.amazonaws.com%2F4d94f956-1570-11e7-b0c1-37e417ee6c76?source=ig&fit=scale-down&width=1200',
+  // socialHeadline: '',
+  // socialSummary: 'The UK's Article 50 notification letter, annotated',
+
+  // TWITTER
+twitterImage: 'https://www.ft.com/__origami/service/image/v2/images/raw/http%3A%2F%2Fcom.ft.imagepublish.prod.s3.amazonaws.com%2F4d94f956-1570-11e7-b0c1-37e417ee6c76?source=ig&fit=scale-down&width=1800',
+twitterCreator: '@ft',
+tweetText:  'The the EU’s Brexit negotiation guidelines with comments from the FT',
+twitterHeadline:  'The the EU’s Brexit negotiation guidelines, annotated',
+
+  // FACEBOOK
+ facebookImage: 'https://www.ft.com/__origami/service/image/v2/images/raw/http%3A%2F%2Fcom.ft.imagepublish.prod.s3.amazonaws.com%2F4d94f956-1570-11e7-b0c1-37e417ee6c76?source=ig&fit=scale-down&width=1800',
+ facebookHeadline: 'The the EU’s Brexit negotiation guidelines, annotated',
+
+  tracking: {
+
+    /*
+
+    Microsite Name
+
+    e.g. guffipedia, business-books, baseline.
+    Used to query groups of pages, not intended for use with
+    one off interactive pages. If you're building a microsite
+    consider more custom tracking to allow better analysis.
+    Also used for pages that do not have a UUID for whatever reason
+    */
+    // micrositeName: '',
+
+    /*
+    Product name
+
+    This will usually default to IG
+    however another value may be needed
+    */
+    // product: '',
+  },
+});
